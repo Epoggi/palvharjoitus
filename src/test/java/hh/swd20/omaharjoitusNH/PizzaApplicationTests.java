4cff866347c68e1618aab7e3f2c0761747d2fb4d
Epoggi <11327084+Epoggi@users.noreply.github.com>
2020-04-02 14:00:19 +0300

    Eka committi harjoitustyölle

---
+++ src/test/java/hh/swd20/omaharjoitusNH/PizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.swd20.omaharjoitusNH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
